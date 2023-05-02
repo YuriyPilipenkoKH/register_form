8dc24f2831f3319c1fe3b090a326152677ea4297
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-02 20:52:27 +0300

    val

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import './css/styles.css'
+import { isValid } from './utils/utils'
 
 const form = document.getElementById('form')
-console.log(form);
+const input = document.getElementById('q')
+const submitBtn = document.querySelector('.mui-btn')
 
-console.log('firebase');
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', inputHandler)
+// submitBtn.disabled = true
+
+function inputHandler ( ) {
+    submitBtn.disabled = !isValid(input.value)
+}
+
+function submitFormHandler(e) {
+  e.preventDefault()
+
+  if(isValid(input.value)) {
+  
+    const question = {
+        text: input.value.trim(),
+        date: new Date().toJSON()
+    }
+
+    console.log(question);
+    input.value = ''
+    input.className = ''
+    submitBtn.disabled = false
+  }
+}
